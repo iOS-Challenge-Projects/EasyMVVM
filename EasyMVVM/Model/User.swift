69084545fb155d1d09fe62e46a1178ec6b21ec17
fritzgt <tuanis@live.com>
2020-08-17 11:52:38 +0300

    added file structure and storyboard

---
+++ EasyMVVM/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  EasyMVVM
+//
+//  Created by FGT MAC on 8/17/20.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import Foundation
+
+
+struct User {
+    var name: String
+    var age: Int
+    
+}
